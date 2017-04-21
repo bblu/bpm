97d8f5b3959be7a2983e3d4e1fe9dec2296ee1f6
bblu <gmwblu@gmail.com>
2017-04-21 20:06:14 +0800

    view engine handlebars

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+var app = express();
+//if use 80 port maybe unhandled error event.js 160
+app.set('port',process.env.PORT || 3000);
+app.set('view engine','html');
+app.engine('html',require('hbs').__express);
+
+app.get('/',function(req,res){
+    res.render('home',{fortune:"fortune"});
+});
+
+app.get('/about',function(req,res){
+    res.render('about',{fortune:"fortune"});
+});
+app.use(function(req, res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started at ' + app.get('port'));
+});
